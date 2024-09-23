a8bbe5e2fb9b94e38272eee7d4e0d4203c8b491f
tdch3n <145699386+tdch3n@users.noreply.github.com>
2024-09-23 10:45:21 +0000

    Updated Streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,53 @@
 import streamlit as st
+import pandas as pd
 
-st.title("🎈 My new app")
+st.title("FPL team helper ⚽")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "A tool to assist with transfers and captain selection.\
+        To begin, please enter your team in the sidebar."
 )
+
+
+# ------- Allow users to enter team --------
+# Load all player names
+df = pd.read_csv('data/merged_gw.csv')
+player_names = df['name'].unique().copy()
+
+
+# ------- Sidebar for team entry --------
+st.sidebar.header('Enter your team')
+starter_selections = []
+sub_selections = []
+
+# Staring lineup
+st.sidebar.subheader('Starting 11')
+
+for i in range(11):
+    remaining_players = [player for player in player_names\
+                          if player not in starter_selections\
+                              and player not in sub_selections]
+
+    player = st.sidebar.selectbox(
+        'Enter player', 
+        options = remaining_players,
+        key = f'starter_{i}',
+        help="Start typing to search for a player"
+        )
+    starter_selections.append(player)
+
+# Subs
+st.sidebar.subheader('Subs')
+
+for i in range(4):
+    remaining_players = [player for player in player_names\
+                          if player not in starter_selections\
+                              and player not in sub_selections]
+
+    player = st.sidebar.selectbox(
+        'Enter player', 
+        options = remaining_players,
+        key = f'sub_{i}',
+        help="Start typing to search for a player"
+        )
+    sub_selections.append(player)
+
